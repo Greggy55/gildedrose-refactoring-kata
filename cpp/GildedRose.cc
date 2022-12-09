b6ee7bdb038bff92719bcf2744e2bbf3d8ade6a4
Greggy55 <90200788+Greggy55@users.noreply.github.com>
2022-12-09 21:49:47 +0100

    refactor conditions
    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert" && items[i].quality < 50)

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -25,22 +25,15 @@ void GildedRose::updateQuality()
         {
             items[i].quality++;
 
-            if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+            if (items[i].name == "Backstage passes to a TAFKAL80ETC concert" && items[i].quality < 50)
             {
-                if (items[i].sellIn < 11)
-                {
-                    if (items[i].quality < 50)
-                    {
-                            items[i].quality++;
-                    }
-                }
-
                 if (items[i].sellIn < 6)
                 {
-                    if (items[i].quality < 50)
-                    {
-                        items[i].quality++;
-                    }
+                    items[i].quality += 2;
+                }
+                else if (items[i].sellIn < 11)
+                {
+                     items[i].quality++;
                 }
             }
         }
@@ -66,7 +59,7 @@ void GildedRose::updateQuality()
                 }
                 else
                 {
-                    items[i].quality = items[i].quality - items[i].quality;
+                    items[i].quality -= items[i].quality;
                 }
             }
             else if (items[i].quality < 50)
